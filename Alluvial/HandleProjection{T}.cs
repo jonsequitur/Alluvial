ba96c78448697ce12db07ea104b7c0f857f15de1
Jon Sequeira <jonsequeira@gmail.com>
2015-02-12 17:54:28 -0800

    a couple of fix comments

--- Alluvial/HandleProjection{T}.cs
+++ Alluvial/HandleProjection{T}.cs
@@ -3,6 +3,9 @@ using System.Threading.Tasks;
 
 namespace Alluvial
 {
+    // FIX: rename FetchAndSaveProjection
+    // FIX: rename CallAggregatorPipeline
+
     public delegate Task FetchAndSaveProjection<TProjection>(
         string streamId,
         CallAggregatorPipeline<TProjection> aggregate);
