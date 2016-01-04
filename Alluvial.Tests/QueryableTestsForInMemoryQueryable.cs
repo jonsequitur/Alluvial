aa932ca4ef76b09f77890aa7ac44846da36a809d
Jon Sequeira <jonsequeira@gmail.com>
2016-01-04 15:55:41 -0800

    make WithinPartition work for in-memory queryables

---
+++ Alluvial.Tests/QueryableTestsForInMemoryQueryable.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Alluvial.Tests
+{
+    public class QueryableTestsForInMemoryQueryable : QueryableTests
+    {
+        private readonly List<Event> events = new List<Event>();
+
+        protected override async Task WriteEvents(Func<int, Event> createEvent, int howMany = 100)
+        {
+            for (var i = 1; i <= howMany; i++)
+            {
+                events.Add(createEvent(i));
+            }
+        }
+
+        protected override IQueryable<Event> Events()
+        {
+            return events.AsQueryable();
+        }
+    }
+}
