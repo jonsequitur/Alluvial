1689565388991226029f72f5ee3783826875fb48
Jon Sequeira <jonsequeira@gmail.com>
2016-02-15 19:31:01 -0800

    ignore unfinished test

--- Alluvial.Tests/MultiStreamCatchupTests.cs
+++ Alluvial.Tests/MultiStreamCatchupTests.cs
@@ -515,6 +515,7 @@ namespace Alluvial.Tests
             getCount.Should().Be(1);
         }
 
+        [Ignore]
         [Test]
         public async Task A_backoff_can_be_specified_to_poll_less_frequently_when_data_is_slow()
         {
