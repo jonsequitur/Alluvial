44fc952d6f4671faee70b2ee54468e8cdfa3568f
Jon Sequeira <jonsequeira@gmail.com>
2016-05-03 06:43:37 -0700

    wip

--- Alluvial.Tests/StreamBuilderTests.cs
+++ Alluvial.Tests/StreamBuilderTests.cs
@@ -13,7 +13,7 @@ namespace Alluvial.Tests
         {
             IPartitionedStream<string, int, Guid> partitioned1;
             partitioned1 = new StreamBuilder<string>("partitioned-1")
-                .Cursor(_ => _.StartsAt(() => Cursor.New<int>()))
+                .Cursor(_ => _.StartsAt(() => Cursor.New(0)))
                 .Advance((q, b) => q.Cursor.AdvanceTo(b.Count()))
                 .Partition(_ => _.By<Guid>())
                 .CreateStream();
